49ef24d6fe5cd44576349a7d885986b1fa0358e6
eric <eric.zsp@hotmail.com>
2020-04-01 18:24:53 +0800

    版本号

--- webpack.config.js
+++ webpack.config.js
@@ -99,7 +99,7 @@ module.exports = {
           compress: {
             warnings: false,
             drop_debugger: true,
-            drop_console: false
+            drop_console: true
           }
         }
       }),
