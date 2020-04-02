d099d13ead33bf303a40df31bb8c8a0f325abb42
eric <eric.zsp@hotmail.com>
2020-04-02 08:59:33 +0800

    增加全局loading

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,5 +1,22 @@
 import { dialogFunc } from './core.js';
 import "../css/dialog.css";
+var loading_dia;
+JoyDialog.showLoading=function(){
+    if(!loading_dia){
+        var jdia=JoyDialog({
+            type : 'loading',
+            infoText: '',
+            autoClose:0
+        });
+        loading_dia = jdia;
+    }
+}
+JoyDialog.hideLoading=function(){
+    if(loading_dia){
+        loading_dia.close();
+        loading_dia = null;        
+    }
+}
 export function JoyDialog(options){
    return dialogFunc(options)
 }
