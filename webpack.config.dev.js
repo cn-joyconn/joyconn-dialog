343bb500e7436cd969ede9a117453f475534e1c7
eric <eric.zsp@hotmail.com>
2025-06-13 10:56:02 +0800

    20250613

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,8 +5,8 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  // mode: 'development',
+  // mode: 'production',
+  mode: 'development',
   entry: ['./index.js'],
   output: {
     path: path.resolve(__dirname, '.'),
